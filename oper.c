95da0a731dd5efd7d9f4653d9562001421d59aca
Ubuntu <Eseosa@myVirM.5jv4spq3soou1bihmte2k2lixa.cwx.internal.cloudapp.net>
2018-11-13 21:55:35 +0000

    updated the program for math functions

--- oper.c
+++ oper.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
 int main(){
-int a, b, sum;
+int a, b, sum, subtract, multiply;
+float divide;
 printf("What two numbers do you want to add? \n");
 scanf("%d %d", &a, &b);
 sum = a + b;
+subtract = a - b;
+multiply = a * b;
+divide = a / b; 
 printf("Sum of the numbers = %d\n", sum);
+printf("difference btw the numbers = %d\n", subtract);
+printf("multiplication of the numbers = %d\n", multiply);
+printf("Division of first number by second number = %.2f\n", divide);
+return 0;
 }
