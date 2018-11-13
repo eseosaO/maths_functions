ac69de595d4c4055f72772629dedf470ee9cd7fd
Ubuntu <Eseosa@myVirM.5jv4spq3soou1bihmte2k2lixa.cwx.internal.cloudapp.net>
2018-11-13 21:36:19 +0000

    c program with add function

---
+++ oper.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+int a, b, sum;
+printf("What two numbers do you want to add? \n");
+scanf("%d %d", &a, &b);
+sum = a + b;
+printf("Sum of the numbers = %d\n", sum);
+}
