0eb12c2bd302e330f274745fe11e668637e1d4fd
Ubuntu <Eseosa@myVirM.5jv4spq3soou1bihmte2k2lixa.cwx.internal.cloudapp.net>
2018-11-14 22:00:44 +0000

    now including forgotten file

--- oper.c
+++ oper.c
@@ -1,16 +1,31 @@
 #include<stdio.h>
 int main(){
-int a, b, sum, subtract, multiply;
-float divide;
-printf("What two numbers do you want to add? \n");
-scanf("%d %d", &a, &b);
-sum = a + b;
+int a ,x , b = 2, count, sum =0 , min, max,subtract, multiply;
+float divide, average;
+
+printf("Please enter random number\n");
+scanf("%d", &a);
+
+printf("Please enter minimum\n");
+scanf("%d", &min);
+
+printf("Enter maximum\n");
+scanf("%d", &max);
+
+printf("enter %d number\n", max);
+for(count = 1; count <= max; count++){
+scanf("%d", &x);
+sum = sum + x;
+}
+
+average = (float)sum/max;
 subtract = a - b;
 multiply = a * b;
-divide = a / b; 
+divide = (float)a / b; 
 printf("Sum of the numbers = %d\n", sum);
-printf("difference btw the numbers = %d\n", subtract);
-printf("multiplication of the numbers = %d\n", multiply);
-printf("Division of first number by second number = %.2f\n", divide);
+printf("Average of the numbers = %.2f\n", average);
+printf("difference btw a and b = %d\n", subtract);
+printf("multiplication of a and b = %d\n", multiply);
+printf("Division of a by b = %.2f\n", divide);
 return 0;
 }
