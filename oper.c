80f6e2354e48c37e5fb4020cb8b8929ba8aa1344
Ubuntu <Eseosa@myVirM.5jv4spq3soou1bihmte2k2lixa.cwx.internal.cloudapp.net>
2018-11-17 21:51:15 +0000

    new function added

--- oper.c
+++ oper.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+
+int mod(int x, int y){
+return (x % y);
+}
 int main(){
 int a, x, b = 2, count, sum =0 , min, max,subtract, multiply;
 float divide, average;
@@ -21,11 +25,13 @@ sum = sum + x;
 average = (float)sum/max;
 subtract = a - b;
 multiply = a * b;
-divide = (float)a / b; 
+divide = (float)a / b;
+ 
 printf("Sum of the numbers = %d\n", sum);
 printf("Average of the numbers = %.2f\n", average);
 printf("difference btw a and b = %d\n", subtract);
 printf("multiplication of a and b = %d\n", multiply);
 printf("Division of a by b = %.2f\n", divide);
+printf("reminder value between a and b is %d\n", mod(a,b));
 return 0;
 }
